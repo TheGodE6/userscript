93c580f6680966f1b8c8455ebdf0cd9177132452
TheGodE6 <110555637+TheGodE6@users.noreply.github.com>
2025-04-12 08:24:58 -0400

    Update bypass-vip.user.js

--- bypass-vip.user.js
+++ bypass-vip.user.js
@@ -1,228 +1,125 @@
 // ==UserScript==
-// @name          BYPASS.VIP BYPASSER
-// @namespace     bypass.vip
-// @version       1.4.1
-// @author        bypass.vip
-// @description   Bypass ad-links using the bypass.vip API and get to your destination without ads!
-// @match         *://mega-guy.com/*
-// @match         *://loot-link.com/*
-// @match         *://best-links.org/*
-// @match         *://loot-links.com/*
-// @match         *://megaspremium.com/*
-// @match         *://lootdest.com/*
-// @match         *://direct-links.net/*
-// @match         *://risquemega.com/*
-// @match         *://onlyfriends.club/*
-// @match         *://onepiecered.co/*
-// @match         *://multileaks.com/*
-// @match         *://luvsquad-links.com/*
-// @match         *://lootdest.org/*
-// @match         *://free-leaks.com/*
-// @match         *://goldmega.online/*
-// @match         *://realiukzemydre.com/*
-// @match         *://kmendation.com/*
-// @match         *://lootlinks.co/*
-// @match         *://onlyshare.info/*
-// @match         *://leakplugs.com/*
-// @match         *://nswfbox.com/*
-// @match         *://eofmukindwo.com/*
-// @match         *://op-packs.com/*
-// @match         *://thedetective.online/*
-// @match         *://forlinkmeg.com/*
-// @match         *://thegoatpack.org/*
-// @match         *://of-leaks.xyz/*
-// @match         *://links.spacebin.in/*
-// @match         *://supermeg.com/*
-// @match         *://onlyfanessereloaded.com/*
-// @match         *://fanzleaks.com/*
-// @match         *://cemendemons.com/*
-// @match         *://premiummegaz.com/*
-// @match         *://thepremium.online/*
-// @match         *://pasteebins.com/*
-// @match         *://baddiezcentral.com/*
-// @match         *://megazone.website/*
-// @match         *://leak-pragmatic.com/*
-// @match         *://megadumpz.com/*
-// @match         *://thhaven.net/*
-// @match         *://meg4fans.com/*
-// @match         *://depravityweb.co/*
-// @match         *://discordlink.cc/*
-// @match         *://megashub.co/*
-// @match         *://streamergirls.org/*
-// @match         *://your-leaks.com/*
-// @match         *://onlylinksmegas.xyz/*
-// @match         *://pypy.spacebin.in/*
-// @match         *://pypy.in/*
-// @match         *://luvsquad-links.cmo/*
-// @match         *://beast.net.in/*
-// @match         *://hook.beast.net.in/*
-// @match         *://all-fans.online/*
-// @match         *://fansmega.com/*
-// @match         *://worldpacks.co/*
-// @match         *://night-hub.online/*
-// @match         *://dailyofleaks.com/*
-// @match         *://pkofs.com/*
-// @match         *://offree90.com/*
-// @match         *://megadropz.com/*
-// @match         *://onlyfunlink.com/*
-// @match         *://direct-links.org/*
-// @match         *://leaks4you.com/*
-// @match         *://onlylinksmegas.com/*
-// @match         *://direct-link.net/*
-// @match         *://downbadleaks.com/*
-// @match         *://onlyfanscloud.com/*
-// @match         *://missleakz.com/*
-// @match         *://leakszone.online/*
-// @match         *://links-loot.com/*
-// @match         *://holyfanslinks.com/*
-// @match         *://utopianleaks.com/*
-// @match         *://megavip.store/*
-// @match         *://drlinker.com/*
-// @match         *://baddiesheaven.com/*
-// @match         *://of4lm-links.com/*
-// @match         *://holedonly.store/*
-// @match         *://lootlinks.com/*
-// @match         *://free-content.pro/*
-// @match         *://milky-center.com/*
-// @match         *://megaofs.com/*
-// @match         *://bleleadersto.com/*
-// @match         *://link-target.org/*
-// @match         *://daughablelea.com/*
-// @match         *://mymegalinks.com/*
-// @match         *://heroslut.com/*
-// @match         *://tonordersitye.com/*
-// @match         *://vip-linknetwork.com/*
-// @match         *://birdbiss.com/*
-// @match         *://loot-labs.com/*
-// @match         *://lootlabs.com/*
-// @match         *://link-hub.net/*
-// @match         *://locconnect.com/*
-// @match         *://premiumstashdrop.com/*
-// @match         *://lootdest.info/*
-// @match         *://of-area.com/*
-// @match         *://link-target.net/*
-// @match         *://megalnk.com/*
-// @match         *://lootlink.org/*
-// @match         *://nsfwcherry.com/*
-// @match         *://cherrypacks.online/*
-// @match         *://hotstars-leaks.com/*
-// @match         *://onlymega.co/*
-// @match         *://hanimeturks.com/*
-// @match         *://dailyleakz.com/*
-// @match         *://content-hub.club/*
-// @match         *://dailyadultmegas.com/*
-// @match         *://oui-chu.com/*
-// @match         *://megalinks.one/*
-// @match         *://starleakz.com/*
-// @match         *://babeslink.click/*
-// @match         *://ofgirls3x.com/*
-// @match         *://pnp-drops.me/*
-// @match         *://nsfw-paradise.club/*
-// @match         *://key-access.co/*
-// @match         *://leaksmix.com/*
-// @match         *://sensual-leaks.com/*
-// @match         *://ofhub-leaks.com/*
-// @match         *://onlyforfan.online/*
-// @match         *://sweetjuice-mega.com/*
-// @match         *://crip-hub.com/*
-// @match         *://leaksfreeday.com/*
-// @match         *://thotshaven.online/*
-// @match         *://mega-leaked.com/*
-// @match         *://beautifulgirls.social/*
-// @match         *://tavernleaks.com/*
-// @match         *://nudeleaksteenz.com/*
-// @match         *://slutywet.com/*
-// @match         *://leakutopia.site/*
-// @match         *://secret-packs.com/*
-// @match         *://attiktok22.com/*
-// @match         *://pancakes-leaks.com/*
-// @match         *://diamond-leaks.com/*
-// @match         *://lewd-leaks.com/*
-// @match         *://ftbleaks.net/*
-// @match         *://thepytheaven.org/*
-// @match         *://of-region.online/*
-// @match         *://admiregirls-byme.com/*
-// @match         *://mzehoney12.com/*
-// @match         *://herplace.online/*
-// @match         *://megaplugleaks.com/*
-// @match         *://of-kingdom.com/*
-// @match         *://onlyfansmegafolder.com/*
-// @match         *://links.spacebin.in/*
-// @match         *://adfoc.us/*
-// @match         *://boost.ink/*
-// @match         *://cuty.io/*
-// @match         *://www.cuty.io/*
-// @match         *://cety.app/*
-// @match         *://www.cety.app/*
-// @match         *://linkvertise.com/*/*
-// @match         *://mboost.me/*
-// @match         *://bst.gg/*
-// @match         *://booo.st/*
-// @match         *://mendationforc.info/*
-// @match         *://paster.so/*
-// @match         *://paster.gg/*
-// @match         *://rekonise.com/*
-// @match         *://social-unlock.com/*
-// @match         *://www.social-unlock.com/*
-// @match         *://socialwolvez.com/*
-// @match         *://sub2get.com/*
-// @match         *://www.sub2get.com/*
-// @match         *://sub2unlock.com/*
-// @match         *://sub2unlock.io/*
-// @match         *://sub2unlock.net/*
-// @match         *://sub2unlock.online/*
-// @match         *://sub2unlock.top/*
-// @match         *://sub4unlock.pro/*
-// @match         *://sub4unlock.com/*
-// @match         *://sub4unlock.io/*
-// @match         *://subfinal.com/*
-// @match         *://unlocknow.net/*
-// @match         *://v.gd/*
-// @match         *://work.ink/*
-// @match         *://www.work.ink/*
-// @match         *://workink.net/*
-// @match         *://r.work.ink/*
-// @match         *://workink.one/*
-// @match         *://workink.me/*
-// @match         *://ytsubme.com/*
-// @match         *://www.ytsubme.com/*
-// @match         *://esohasl.net/*
-// @match         *://rbscripts.net/*
-// @match         *://link.rbscripts.net/*
-// @downloadURL   https://raw.githubusercontent.com/bypass-vip/userscript/master/bypass-vip.user.js
-// @updateURL     https://raw.githubusercontent.com/bypass-vip/userscript/master/bypass-vip.user.js
-// @homepageURL   https://bypass.vip
-// @icon          https://www.google.com/s2/favicons?domain=bypass.vip&sz=64
-// @run-at document-start
+// @name        Bypass Key System [Premium]
+// @homepageURL https://discord.gg/gFNAH7WNZj
+// @namespace   https://tampermonkey.net/
+// @version     3.7.3
+// @description A userscript designed to bypass restrictions and streamline user experiences by automating bypass processes for various stages and challenges
+// @author      Bacon But Pro
+// @match       *://linkvertise.com/*/*
+// @match       *://linkvertise.com/?iwantbypass=*
+// @match       *://loot-link.com/s?*
+// @match       *://loot-links.com/s?*
+// @match       *://lootlink.org/s?*
+// @match       *://lootlinks.co/s?*
+// @match       *://lootdest.info/s?*
+// @match       *://lootdest.org/s?*
+// @match       *://lootdest.com/s?*
+// @match       *://links-loot.com/s?*
+// @match       *://linksloot.net/s?*
+// @match       *://socialwolvez.com/*
+// @match       *://rekonise.com/*
+// @match       *://mboost.me/a/*
+// @match       *://*.keyrblx.com/*
+// @match       *://mobile.codex.lol/*
+// @match       *://pandadevelopment.net/getkey?*
+// @match       *://spdmteam.com/key-system*
+// @match       *://*/recaptcha/*
+// @match       *://*.hcaptcha.com/*hcaptcha-challenge*
+// @match       *://*.hcaptcha.com/*checkbox*
+// @match       *://*.hcaptcha.com/*captcha*
+// @match       *://bot-hosting.net/panel/*
+// @match       *://trigonevo.com/getkey/?hwid=*
+// @match       *://trigonevo.fun/whitelist/*
+// @match       *://work.ink/*/*
+// @match       *://workink.net/*/*
+// @match       *://paster.so/*
+// @match       *://sub2unlock.com/*
+// @match       *://*.sub2get.com/*
+// @match       *://boost.ink/*
+// @match       *://social-unlock.com/*
+// @match       *://getkey.farrghii.com/*
+// @match       *://dusarisalary.com/*
+// @match       *://bloggingdaze.com/*
+// @match       *://sub4unlock.com/*
+// @match       *://mendationforc.info/*
+// @match       *://flux.li/android/external/*
+// @match       *://ads.luarmor.net/*
+// @match       *://bstlar.com/*
+// @match       *://yeumoney.com/*
+// @match       *://auth.platoboost.com/*
+// @match       *://auth.platoboost.net/*
+// @match       *://auth.platorelay.com/*
+// @match       *://keyguardian.org/*
+// @match       *://krnl.cat/checkpointv2/*
+// @resource    notifCss https://cdn.jsdelivr.net/gh/f3oall/awesome-notifications/dist/style.css
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js
+// @require     https://update.greasyfork.org/scripts/484331/1308737/customnotifications.js
+// @require     https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/API.js
+// @require     https://cdn.rawgit.com/ricmoo/aes-js/e27b99df/index.js
+// @require     https://github.com/BaconButPro/ron12373/raw/Premium/Bypass-Userscript/Main.js
+// @run-at      document-end
+// @grant       GM_getResourceText
+// @grant       GM_xmlhttpRequest
+// @grant       GM_notification
+// @grant       GM_openInTab
+// @grant       GM_addStyle
+// @grant       GM_setClipboard
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @license     Bacon
+// @connect     linkvertise.com
+// @connect     short-jambo.com
+// @connect     api.codex.lol
+// @connect     cdn.jsdelivr.net
+// @connect     spdmteam.com
+// @connect     api.keyrblx.com
+// @connect     keyrblx.com
+// @connect     auth.platoboost.net
+// @connect     ads.luarmor.net
+// @connect     pandadevelopment.net
+// @connect     loot-link.com
+// @connect     loot-links.com
+// @connect     lootlink.org
+// @connect     lootlinks.co
+// @connect     lootdest.info
+// @connect     lootdest.org
+// @connect     lootdest.com
+// @connect     links-loot.com
+// @connect     linksloot.net
+// @connect     auth.platorelay.com
+// @connect     auth.platoboost.com
+// @icon        https://cdn141.picsart.com/351217840073211.png
+// @downloadURL https://github.com/BaconButPro/ron12373/raw/Premium/Install-Userscript/Bypass.user.js
+// @updateURL https://github.com/BaconButPro/ron12373/raw/Premium/Install-Userscript/Bypass.user.js
 // ==/UserScript==
 
-(async () => {
-    const config = {
-        time: 4, // Wait time to avoid detections (Increase this to 30+ seconds to be extra safe from key system bypass protections)
-        key: '' //Premium key if you have one
+function APIKEY() {
+    const APIKEY = '5f3f05bf-fe86-43aa-ad02-4ba9b45b7940';
+    return APIKEY;
+}
+
+function config() {
+    const fluxus_copy = false; // enable to auto copy Fluxus key
+    const keyrblx_copy = false; // enable to auto copy Keyrblx key
+    const panda_copy = false; // enable to auto copy Pandadevelopment key
+    const platoboost_copy = false; // enable to auto copy Platoboost key
+    const keyguardian_copy = false; // enable to auto copy Keyguardian key
+    const krnl_copy = false; // enable to auto copy Krnl key
+    return { fluxus_copy, keyrblx_copy, panda_copy, platoboost_copy, keyguardian_copy, krnl_copy };
+}
+
+function time_config() {
+    return {
+        ads_luarmor: 40, // set delay bypass for ads.luarmor.net
+        linkvertise: 1, // set delay bypass for linkvertise.com
+        lootlabs: 1 // set delay bypass for lootlabs
     };
+}
 
-    const originalCreateElement = document.createElement.bind(document);
-    document.createElement = function(elementName) {
-        const element = originalCreateElement(elementName);
-        if (elementName.toLowerCase() === 'script') {
-            element.setAttribute('type', 'text/plain');
-        }
-        return element;
-    };
-
-    document.documentElement.innerHTML = `<html><head><title>BYPASS.VIP USERSCRIPT</title><link rel="stylesheet" href="https:///bypass.vip/assets/css/styles.css"></head><body class="userscript"><h1>bypass.vip userscript</h1><h2>redirecting...</h2></body></html>`;
-
-    const urlParams = new URLSearchParams(window.location.search);
-    const redirectUrl = urlParams.get('redirect')
-
-    if (redirectUrl && redirectUrl.includes('https://flux.li/android/external/main.php')) {
-        document.body.innerHTML = `<h1>bypass.vip userscript</h1><h2>Fluxus implements some extra security checks to detect bypasses so we can't automatically redirect you.</h2><h3><a href="${redirectUrl}">Click here to redirect</a></h3>`;
-        return;
-    } else if(redirectUrl) {
-        location.href = redirectUrl
-        return;
-    }
-    location.href = `https://bypass.vip/userscript.html?url=${encodeURIComponent(location.href)}&time=${config.time}&key=${config.key}`
-    return;
-})();
+function others() {
+    const linkvertise_Premium = false; // enable if have linkvertise premium only
+    const fallback_checking = false; // Set to true if the checking key is failed
+    const clientside_delta = false; // enable delta client side bypass (Beta)
+    const fallback_api = false; // use the fallback api instead primary
+    return { linkvertise_Premium, fallback_checking, clientside_delta, fallback_api };
+}
